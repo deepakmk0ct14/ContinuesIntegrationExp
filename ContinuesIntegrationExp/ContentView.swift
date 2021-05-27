0e4dd8673195e7cd6314dd97844b77febdf8bb66
deepak <deepakmk.ec@gmail.com>
2021-05-27 21:05:14 +0530

    Initial Commit

---
+++ ContinuesIntegrationExp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ContinuesIntegrationExp
+//
+//  Created by Deepak on 5/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ContinuesIntegrationExp/ContinuesIntegrationExpApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContinuesIntegrationExpApp.swift
+//  ContinuesIntegrationExp
+//
+//  Created by Deepak on 5/27/21.
+//
+
+import SwiftUI
+
+@main
+struct ContinuesIntegrationExpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
